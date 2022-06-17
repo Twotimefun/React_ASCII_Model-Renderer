8c7e9e5f755f066b93b5d1daf3cd078f020a39ee
Twotimefun <twotimefun@gmail.com>
2022-06-17 18:56:30 +1000

    Commit 2
    Establishing ASCII converter environment

--- src/App.js
+++ src/App.js
@@ -1,10 +1,100 @@
+// Threejs example: threejs.org/examples/?q=asc#webgl_effects_ascii
 
-function App() {
+import { useEffect, useRef, useLayoutEffect, useState, useMemo } from 'react'
+import { Canvas, useFrame, useThree } from '@react-three/fiber'
+import { OrbitControls, useCursor, useGLTF } from '@react-three/drei'
+import { AsciiEffect } from 'three-stdlib'
+import * as THREE from 'three'
+
+export default function App() {
+  const [clicked, click] = useState(false)
+
   return (
-    <div className="App">
-      Init
-    </div>
-  );
+    <Canvas>
+      <color attach="background" args={['black']} />
+      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+      <pointLight position={[-10, -10, -10]} />
+      <ModelRender />
+      <OrbitControls />
+      <AsciiRenderer invert />
+    </Canvas>
+  )
 }
 
-export default App;
+function Urus(props) {
+  const { scene, nodes, materials } = useGLTF('/lambo.glb')
+  useLayoutEffect(() => {
+    scene.traverse((obj) => obj.type === 'Mesh' && (obj.receiveShadow = obj.castShadow = true))
+    Object.assign(nodes.wheel003_020_2_Chrome_0.material, { metalness: 0.9, roughness: 0.4, color: new THREE.Color('#020202') })
+    Object.assign(materials.WhiteCar, { roughness: 0.0, metalness: 0.3, emission: new THREE.Color('#500000'), envMapIntensity: 0.5 })
+  }, [scene, nodes, materials])
+  return <primitive object={scene} {...props} />
+}
+
+function LamboC(props) {
+  const { scene, nodes, materials } = useGLTF('/lamboC.glb')
+  useLayoutEffect(() => {
+    scene.traverse((obj) => obj.type === 'Mesh' && (obj.receiveShadow = obj.castShadow = true))
+  }, [scene, nodes, materials])
+  return <primitive object={scene} {...props} />
+}
+
+function ModelRender(props) {
+  const ref = useRef()
+  // const [clicked, click] = useState(false)
+  const [hovered, hover] = useState(false)
+  useCursor(hovered)
+  useFrame((state) => {
+    ref.current.rotation.y += 0.006
+  })
+  // useFrame((state, delta) => (ref.current.rotation.y = ref.current.rotation.y += delta / 32))
+  
+  return (
+    <mesh
+      {...props}
+      ref={ref}
+      scale={1}
+      // onClick={() => click(!clicked)}
+      onPointerOver={() => hover(true)}
+      onPointerOut={() => hover(false)}>
+      <Urus scale={0.01} />
+      {/* <LamboC scale={0.55} /> */}
+      <meshStandardMaterial color="orange" />
+    </mesh>
+  )
+}
+
+function AsciiRenderer({ renderIndex = 1, characters = ' .:-+*=%@#', ...options }) {
+  // Reactive state
+  const { size, gl, scene, camera } = useThree()
+
+  // Create effect
+  const effect = useMemo(() => {
+    const effect = new AsciiEffect(gl, characters, options)
+    effect.domElement.style.position = 'absolute'
+    effect.domElement.style.top = '0px'
+    effect.domElement.style.left = '0px'
+    effect.domElement.style.color = 'white'
+    effect.domElement.style.backgroundColor = 'black'
+    effect.domElement.style.pointerEvents = 'none'
+    return effect
+  }, [characters, options.invert])
+
+  // Append on mount, remove on unmount
+  useEffect(() => {
+    gl.domElement.parentNode.appendChild(effect.domElement)
+    return () => gl.domElement.parentNode.removeChild(effect.domElement)
+  }, [effect])
+
+  // Set size
+  useEffect(() => {
+    effect.setSize(size.width, size.height)
+  }, [effect, size])
+
+  // Take over render-loop (that is what the index is for)
+  useFrame((state) => {
+    effect.render(scene, camera)
+  }, renderIndex)
+
+  // This component returns nothing, it has no view, it is a purely logical
+}
